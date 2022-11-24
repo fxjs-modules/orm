bb87aa777d1d0f87359330fb307a39fd32848ec5
richardo2016 <richardo2016@users.noreply.github.com>
2022-11-24 11:06:54 +0000

    deploy: cda86760d0f350e56df7d6fa64ed1aad708eee06

---
+++ assets/orm_Adapters.md.769b81ba.lean.js
@@ -0,0 +1,1 @@
+import{_ as n,y as a,x as s,X as t}from"./plugin-vue_export-helper.bb29c578.js";const m='{"title":"Adapters","description":"","frontmatter":{},"headers":[],"relativePath":"orm/Adapters.md","lastUpdated":1669287996859}',e={},p=t("",5),o=[p];function c(r,i,d,l,u,_){return s(),a("div",null,o)}var f=n(e,[["render",c]]);export{m as __pageData,f as default};
