953fc95959ce96990a124794cc07cd01b3bc7bd8
richardo2016 <richardo2016@users.noreply.github.com>
2025-11-20 14:49:50 +0000

    deploy: 5adc9291393171a3f30d625a2f711a6aece6ee07

---
+++ assets/orm_Adapters.md.96f41b4f.js
@@ -0,0 +1,10 @@
+import{_ as a,A as s,W as t,E as p}from"./plugin-vue_export-helper.4be6ef69.js";const k='{"title":"Adapters","description":"","frontmatter":{},"headers":[],"relativePath":"orm/Adapters.md","lastUpdated":1763650177310}',e={};function o(c,n,r,i,l,d){return p(),s("div",null,[...n[0]||(n[0]=[t(`<h1 id="adapters" tabindex="-1">Adapters <a class="header-anchor" href="#adapters" aria-hidden="true">#</a></h1><p>ORM \u5185\u7F6E\u4E86\u5BF9\u90E8\u5206\u6570\u636E\u5E93\u7684\u652F\u6301. \u800C\u901A\u8FC7\u63D0\u4F9B\u4E00\u4E2A\u9002\u914D\u5668, \u53EF\u4EE5\u8BA9 ORM \u5728\u4E0D\u540C\u7684\u6570\u636E\u5E93\u4E0A\u8FD0\u884C.</p><div class="language-js"><pre><code><span class="token function">require</span><span class="token punctuation">(</span><span class="token string">&#39;@fxjs/orm&#39;</span><span class="token punctuation">)</span><span class="token punctuation">.</span><span class="token function">addAdapter</span><span class="token punctuation">(</span><span class="token string">&#39;customdb&#39;</span><span class="token punctuation">,</span> <span class="token punctuation">{</span>
+  <span class="token comment">// ...</span>
+<span class="token punctuation">}</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
+</code></pre></div><p>\u9002\u914D\u5668\u7684\u63A5\u53E3\u5982\u4E0B:</p><div class="language-ts"><pre><code><span class="token keyword">interface</span> <span class="token class-name">IAdatper</span> <span class="token punctuation">{</span>
+  <span class="token comment">/**
+   * @description protocol when connecting to database, e.g. &#39;customdb&#39;
+   */</span>
+  protocol<span class="token operator">:</span> <span class="token builtin">string</span><span class="token punctuation">;</span>
+<span class="token punctuation">}</span>
+</code></pre></div>`,5)])])}var m=a(e,[["render",o]]);export{k as __pageData,m as default};
